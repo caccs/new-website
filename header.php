1fbedcc37da7483af1acaae7eb20dd8b7aae2603
Giovana Morais <giovana.vmorais@gmail.com>
2017-03-31 18:35:57 -0300

    adição do full calendar na header

--- header.php
+++ header.php
@@ -7,3 +7,9 @@
 <link rel="stylesheet" type="text/css" href="css/fonts.css">
 <link rel="stylesheet" type="text/css" href="css/main.css">
 <script type="text/javascript" src="js/main.js"></script>
+<!-- full calendar --> 
+<link rel='stylesheet' href='fullcalendar/fullcalendar.css' />
+<script src='lib/jquery.min.js'></script>
+<script src='lib.moment.min.js'></script>
+<script src='fullcalendar/fullcalendar.js'></script>
+
